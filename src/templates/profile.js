559a4f7b07eaa7792d00da9752ac97b398d477fb
Max Baun <max@d3applications.com>
2018-07-26 13:27:00 -0400

    adding video to profile

--- src/templates/profile.js
+++ src/templates/profile.js
@@ -2,35 +2,68 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import graphql from 'graphql';
 import Img from 'gatsby-image';
+import ReactPlayer from 'react-player';
 
+import Modal from '../components/modal';
 import Seo from '../components/seo';
 import {initPageElements} from '../utils/pageHelpers';
 import {innerHtml} from '../utils/wordpressHelpers';
+import {click} from '../utils/componentHelpers';
 
 export default class ProfileTemplate extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			modalActive: false
+		};
+
+		this.handleModalToggle = this.handleModalToggle.bind(this);
+	}
+
 	static propTypes = {
 		data: PropTypes.object.isRequired,
 		location: PropTypes.object.isRequired
-	}
+	};
 
 	componentDidMount() {
 		initPageElements();
 	}
 
+	handleModalToggle(modalActive) {
+		this.setState({modalActive});
+	}
+
 	render() {
 		const {profile, site} = this.props.data;
+		const {modalActive} = this.state;
 
-		const {facebook, instagram} = profile.acf;
+		const {facebook, instagram, video} = profile.acf;
 
 		const showIcons = (facebook && facebook !== '') || (instagram && instagram !== '');
 
 		return (
 			<div>
-				<Seo
-					currentPage={profile}
-					location={this.props.location}
-					siteMeta={site.siteMeta}
-				/>
+				<Seo currentPage={profile} location={this.props.location} siteMeta={site.siteMeta}/>
+				<Modal
+					showHeader
+					id="profileVideoModal"
+					active={modalActive ? [modalActive] : []}
+					onClose={click(this.handleModalToggle, false)}
+					size="xl"
+				>
+					<div className="single-profile-modal">
+						<ReactPlayer
+							className="single-profile-modal-video"
+							url={video}
+							width="100%"
+							height="100%"
+							style={{
+								margin: '0 auto'
+							}}
+						/>
+					</div>
+				</Modal>
 				<section className="single-profile bg-black">
 					<div className="container">
 						<div className="page-content">
@@ -40,10 +73,8 @@ export default class ProfileTemplate extends Component {
 										<span
 											dangerouslySetInnerHTML={innerHtml(profile.title)} // eslint-disable-line react/no-danger
 											className="color-brand"
-										/>
-										{' '}
-										<span className="title-breaker"/>
-										{' '}
+										/>{' '}
+										<span className="title-breaker"/>{' '}
 										<span
 											dangerouslySetInnerHTML={innerHtml(profile.acf.title)} // eslint-disable-line react/no-danger
 											className="profile-title"
@@ -51,31 +82,36 @@ export default class ProfileTemplate extends Component {
 									</h1>
 								</div>
 								<div className="single-profile-image text-center" style={{maxWidth: 440, margin: '0 auto'}}>
-									<Img
-										sizes={profile.image.localFile.childImageSharp.thumbnail}
-									/>
+									<Img sizes={profile.image.localFile.childImageSharp.thumbnail}/>
+									{video && video !== '' ? (
+										<div className="single-profile-image-overlay">
+											<span className="single-profile-play" onClick={click(this.handleModalToggle, true)}>
+												<span className="fa fa-play"/>
+											</span>
+										</div>
+									) : null}
+								</div>
+								<div className="single-profile-video-btn">
+									<button className="btn btn-primary" onClick={click(this.handleModalToggle, true)}>
+										Watch {profile.title}'s Video
+									</button>
 								</div>
 								<div
 									dangerouslySetInnerHTML={innerHtml(profile.content)} // eslint-disable-line react/no-danger
 									className="single-profile-text"
 								/>
-								{showIcons ?
+								{showIcons ? (
 									<div className="content-block single-profile-social text-right">
-										{facebook && facebook !== '' ?
-											<a href={facebook} target="__blank" className="fa fa-facebook"/> : null
-										}
-										{instagram && instagram !== '' ?
-											<a href={instagram} target="__blank" className="fa fa-instagram"/> : null
-										}
-									</div> : null
-								}
+										{facebook && facebook !== '' ? <a href={facebook} target="__blank" className="fa fa-facebook"/> : null}
+										{instagram && instagram !== '' ? <a href={instagram} target="__blank" className="fa fa-instagram"/> : null}
+									</div>
+								) : null}
 							</div>
 							<section className="text-center title-sm">
 								<div className="section-title">
 									<h1>
 										<span className="color-brand">Testimonials </span>
-										<span className="title-breaker"/>
-										{' '}
+										<span className="title-breaker"/>{' '}
 										<span
 											className="color-white"
 											dangerouslySetInnerHTML={innerHtml(profile.title)} // eslint-disable-line react/no-danger
@@ -95,12 +131,12 @@ export default class ProfileTemplate extends Component {
 }
 
 export const pageQuery = graphql`
-query profileQuery($id: String!) {
-  profile: wordpressWpProfile(id: {eq: $id}) {
-	...Profile
-  }
-  site {
-	...Site
-  }
-}
+	query profileQuery($id: String!) {
+		profile: wordpressWpProfile(id: {eq: $id}) {
+			...Profile
+		}
+		site {
+			...Site
+		}
+	}
 `;
