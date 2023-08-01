edea17f87a758bd2b2149359a5810566438007f4
Max Baun <max@d3applications.com>
2023-08-01 11:22:37 -0400

    site updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,40 +6,36 @@ import Seo from '../components/seo';
 import {initPageElements} from '../utils/pageHelpers';
 
 export default class IndexPage extends React.Component {
-	static propTypes = {
-		data: PropTypes.object.isRequired,
-		location: PropTypes.object.isRequired
-	}
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired
+  };
 
-	componentDidMount() {
-		initPageElements();
-	}
+  componentDidMount() {
+    initPageElements();
+  }
 
-	render() {
-		const {site, homePage} = this.props.data;
+  render() {
+    const {site, homePage} = this.props.data;
 
-		return (
-			<div>
-				<Seo
-					currentPage={homePage}
-					location={this.props.location}
-					siteMeta={site.siteMeta}
-				/>
-				<div
-					dangerouslySetInnerHTML={{__html: homePage.content}} // eslint-disable-line react/no-danger
-				/>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Seo currentPage={homePage} location={this.props.location} siteMeta={site.siteMeta} />
+        <div
+          dangerouslySetInnerHTML={{__html: homePage.content}} // eslint-disable-line react/no-danger
+        />
+      </div>
+    );
+  }
 }
 
 export const pageQuery = graphql`
-query homePageQuery {
-  homePage: wordpressPage(slug: {eq: "home"}) {
-	...Page
+  query homePageQuery {
+    homePage: wordpressPage(slug: {eq: "home"}) {
+      ...Page
+    }
+    site {
+      ...Site
+    }
   }
-  site {
-	...Site
-  }
-}
 `;
